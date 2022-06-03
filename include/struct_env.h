c1d81626ce2a1989d42d4d5d511398962cd0876c
atron <atron@student.42.fr>
2022-06-03 14:50:11 +0200

    clean loc_env

--- include/struct_env.h
+++ include/struct_env.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   struct_env.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbignon <cbignon@student.42.fr>            +#+  +:+       +#+        */
+/*   By: Darkkoll <Darkkoll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 16:05:32 by cbignon           #+#    #+#             */
-/*   Updated: 2022/04/20 11:21:28 by cbignon          ###   ########.fr       */
+/*   Updated: 2022/06/03 14:49:47 by Darkkoll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 typedef struct s_env
 {
-	char	**loc_env;
 	char	**gl_env;
 }	t_env;
 
