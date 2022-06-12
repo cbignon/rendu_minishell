7ec84006c29908a9c8dbb52110b03093c66ea6c6
atron <atron@student.42.fr>
2022-06-12 14:55:17 +0200

    fix ft_split_protec

--- processes/ft_split_protec.c
+++ processes/ft_split_protec.c
@@ -1,28 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
+/*   ft_split_protec.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atron <atron@student.42.fr>                +#+  +:+       +#+        */
+/*   By: Darkkoll <Darkkoll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 13:27:08 by atron             #+#    #+#             */
-/*   Updated: 2021/05/19 19:33:21 by atron            ###   ########.fr       */
+/*   Updated: 2022/06/12 14:32:57 by Darkkoll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "processes_internal.h"
-
-// static void	*ft_freetab(char **tab, size_t size)
-// {
-// 	size_t	x;
-
-// 	x = 0;
-// 	while (x < size)
-// 		gc_delone((void *)&tab[x++], 0);
-// 	gc_delone((void *)&tab, 0);
-// 	return (NULL);
-// }
 
 static int	ft_count_words(char const *str, char c)
 {
@@ -49,21 +38,6 @@ static int	ft_count_words(char const *str, char c)
 	return (words);
 }
 
-// static int	ft_next_word(char const *str, char c, int i)
-// {
-// 	if (i == 0 && str[i] != c && str[i])
-// 		return (i);
-// 	while (str[i] != c && str[i])
-// 	{
-// 		if (str[i] == '"' || str[i] == '\'')
-// 				i += get_closing_quote(&str[i], str[i]);
-// 		i++;
-// 	}
-// 	while (str[i] == c && str[i])
-// 		i++;
-// 	return (i);
-// }
-
 static int	ft_wlength(char const *str, char c, int i)
 {
 	int	length;
@@ -76,8 +50,10 @@ static int	ft_wlength(char const *str, char c, int i)
 		length++;
 	}
 	printf("wlength: %d\n", length);
-	return (length + 1);
+	return (length);
 }
+
+#include <unistd.h>
 
 char	**ft_split_protec(char const *s, char c)
 {
@@ -88,20 +64,15 @@ char	**ft_split_protec(char const *s, char c)
 	int		j;
 
 	words = ft_count_words(s, c);
-	printf("count words: %d\n", words);
 	tab = (char **)malloc_verify(sizeof(char *) * (words + 1));
-	if (!tab)
-		return (NULL);
 	i = -1;
 	ind = 0;
 	while (++i < words && s)
 	{
-		// ind += ft_next_word(s, c, ind);
-		printf("index: %d\n", ind);
 		j = ft_wlength(s, c, ind);
 		tab[i] = (char *)malloc_verify(sizeof(char) * j);
 		tab[i] = ft_substr_cu(s, ind, j);
-		ind += j;
+		ind += j + 1;
 	}
 	tab[i] = NULL;
 	return (tab);
